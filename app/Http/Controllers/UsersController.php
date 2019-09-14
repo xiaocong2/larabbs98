54d9cdc65e50b664b8160ed5f3feb4267581d663
buding <1244685298@qq.com>
2019-09-15 00:40:42 +0800

    用户个人页面

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * 个人中心控制器
+ */
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UsersController extends Controller
+{
+    public function show(User $user)
+    {
+        return view('users.show',compact('user'));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -32,3 +32,6 @@ Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('passw
 Route::get('email/verify', 'Auth\VerificationController@show')->name('verification.notice');
 Route::get('email/verify/{id}', 'Auth\VerificationController@verify')->name('verification.verify');
 Route::get('email/resend', 'Auth\VerificationController@resend')->name('verification.resend');
+
+//个人中心页面
+Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
