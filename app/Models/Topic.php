4f3afbff0b78dc8c0d5b277c70121cb3d7e4bfd0
buding <1244685298@qq.com>
2019-09-19 00:58:19 +0800

    话题列表页面

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -1,8 +1,31 @@
 <?php
-
+/**
+ * 话题控制器
+ */
 namespace App\Models;
 
 class Topic extends Model
 {
+    //允许更新字段
     protected $fillable = ['title', 'body', 'user_id', 'category_id', 'reply_count', 'view_count', 'last_reply_user_id', 'order', 'excerpt', 'slug'];
+
+    /**
+     * 话题和分类1对1关系
+     * 1个话题属于一个分类
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    /**
+     * 一个话题属于一个作者
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
 }
