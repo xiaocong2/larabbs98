102778558dd570cd4362004df3f25dec37ad30b4
buding <1244685298@qq.com>
2019-09-18 22:56:12 +0800

    分类模型和数据

---
+++ app/Models/Category.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * 帖子分类Model
+ */
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    protected $fillable  = [
+        'name','description',
+    ];
+}
