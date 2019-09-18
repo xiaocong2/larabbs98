b568a1cef238fe4a1b9222235f66e4ee027ed5db
buding <1244685298@qq.com>
2019-09-19 00:38:36 +0800

    填充用户和话题数据

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -13,15 +13,19 @@ use Faker\Generator as Faker;
 | This directory should contain each of the model factory definitions for
 | your application. Factories provide a convenient way to generate new
 | model instances for testing / seeding your application's database.
-|
+|sentence方法表示随机获取小段落文本
 */
 
 $factory->define(User::class, function (Faker $faker) {
+    $date_time = $faker->date . " ".$faker->time;
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'password' => '$2y$10$eeOQaMRAxRQ81/i1m7Zy0uTPo6e2PlASZai1BLAXIGpXVzFR7YhC6', // password
         'remember_token' => Str::random(10),
+        'introduction' => $faker->sentence(),
+        'created_at' => $date_time,
+        'updated_at' => $date_time,
     ];
 });
