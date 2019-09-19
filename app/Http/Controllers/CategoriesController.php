bf5d9cf8bcdb3f176cedefd5d1be2643c0b60a18
buding <1244685298@qq.com>
2019-09-20 00:37:51 +0800

    话题排序

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -10,10 +10,16 @@ use Illuminate\Http\Request;
 
 class CategoriesController extends Controller
 {
-    public function show(Category $category)
+    /**
+     * 根据话题分类显示不同的列表
+     * @param Category $category
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function show(Category $category,Request $request, Topic $topic)
     {
         // 读取分类 ID 关联的话题，并按每 20 条分页
-        $topics = Topic::where('category_id',$category->id)->paginate(20);
+        $topics = $topic->withOrder($request->order)->where('category_id',$category->id)->paginate(20);
+//        $topics = Topic::where('category_id',$category->id)->paginate(20);
 
         return view('topics.index',compact('topics','category'));
     }
