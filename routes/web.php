f1f057ad9aca054bc0dd7d50b8838e8439fde4e7
buding <1244685298@qq.com>
2019-09-23 00:23:32 +0800

    用户可以发表评论

--- routes/web.php
+++ routes/web.php
@@ -36,10 +36,13 @@ Route::get('email/resend', 'Auth\VerificationController@resend')->name('verifica
 //个人中心页面
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
 //话题相关路由
-Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::resource('topics', 'TopicsController', ['only' => ['index',  'create', 'store', 'update', 'edit', 'destroy']]);
+Route::get('topics/{topic}/{slug?}', 'TopicsController@show')->name('topics.show');
 //分类下的话题路由
 Route::resource('categories', 'CategoriesController', ['only' => [ 'show']]);
 //上传话题图片
 Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
 
-Route::resource('replies', 'RepliesController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+//回复信息路由
+Route::resource('replies', 'RepliesController', ['only' => [ 'store', 'destroy']]);
+
